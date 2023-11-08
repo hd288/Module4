d9c93b95ac74eae32d6b3b7fbe233dd5fa5f0d8a
hd288 <henrydinhjp288@gmail.com>
2023-11-08 09:21:27 +0700

    add ss6

--- BT-SS6/QuanlySinhVien.sql
+++ BT-SS6/QuanlySinhVien.sql
@@ -2,7 +2,6 @@ DROP DATABASE IF EXISTS QuanLySinhVien;
 CREATE DATABASE IF NOT EXISTS QuanLySinhVien;
 USE QuanLySinhVien;
 
--- Create Students table
 CREATE TABLE Students
 (
     StudentID   INT PRIMARY KEY,
@@ -10,25 +9,21 @@ CREATE TABLE Students
     Age         INT,
     Email       VARCHAR(255)
 );
--- Create Classes table
 CREATE TABLE Classes
 (
     ClassID   INT PRIMARY KEY,
     ClassName VARCHAR(255)
 );
--- Create ClassStudent table
 CREATE TABLE ClassStudent
 (
     StudentID INT,
     ClassID   INT
 );
--- Create Subjects table
 CREATE TABLE Subjects
 (
     SubjectID   INT PRIMARY KEY,
     SubjectName VARCHAR(255)
 );
--- Create Marks table
 CREATE TABLE Marks
 (
     Mark      INT,
@@ -36,8 +31,6 @@ CREATE TABLE Marks
     StudentID INT
 );
 
--- Connect the primary keys using foreign keysa
--- Create foreign keys in the ClassStudent table to connect Students and Classes
 ALTER TABLE ClassStudent
     ADD CONSTRAINT FK_StudentID
         FOREIGN KEY (StudentID)
@@ -46,7 +39,6 @@ ALTER TABLE ClassStudent
         FOREIGN KEY (ClassID)
             REFERENCES Classes (ClassID);
 
--- Create a foreign key in the Marks table to connect Subjects and Students
 ALTER TABLE Marks
     ADD CONSTRAINT FK_SubjectID
         FOREIGN KEY (SubjectID)
@@ -56,7 +48,6 @@ ALTER TABLE Marks
             REFERENCES Students (StudentID);
 
 
--- Insert data into Students table
 INSERT INTO Students (StudentID, StudentName, Age, Email)
 VALUES (1, 'Nguyen Quang An', 18, 'an@yahoo.com'),
        (2, 'Nguyen Cong Vinh', 20, 'vinh@gmail.com'),
@@ -64,14 +55,11 @@ VALUES (1, 'Nguyen Quang An', 18, 'an@yahoo.com'),
        (4, 'Pham Thanh Binh', 25, 'binh@com'),
        (5, 'Nguyen Van Tai Em', 30, 'taiem@sport.vn');
 
-
--- Insert data into Classes table
 INSERT INTO Classes (ClassID, ClassName)
 VALUES (1, 'C0706L'),
        (2, 'C0708G');
 
 
--- Insert data into ClassStudent table
 INSERT INTO ClassStudent (StudentID, ClassID)
 VALUES (1, 1),
        (2, 1),
@@ -80,26 +68,23 @@ VALUES (1, 1),
        (5, 2);
 
 
--- Insert data into Subjects table
 INSERT INTO Subjects (SubjectID, SubjectName)
 VALUES (1, 'SQL'),
        (2, 'Java'),
        (3, 'C'),
        (4, 'Visual Basic');
 
--- Insert data into Marks table
 INSERT INTO Marks (Mark, SubjectID, StudentID)
-VALUES (8, 1, 1), -- SubjectID 1 and StudentID 1 exist
-       (4, 2, 1), -- SubjectID 2 and StudentID 1 exist
-       (9, 1, 1), -- SubjectID 1 and StudentID 1 exist
-       (7, 1, 3), -- SubjectID 1 and StudentID 3 exist
-       (3, 3, 1), -- SubjectID 3 and StudentID 1 exist
-       (4, 3, 5), -- SubjectID 3 and StudentID 5 exist
-       (8, 3, 3), -- SubjectID 3 and StudentID 3 exist
-       (1, 4, 5), -- SubjectID 4 and StudentID 5 exist
-       (3, 4, 2), -- SubjectID 4 and StudentID 2 exist
+VALUES (8, 1, 1),
+       (4, 2, 1),
+       (9, 1, 1),
+       (7, 1, 3),
+       (3, 3, 1),
+       (4, 3, 5),
+       (8, 3, 3),
+       (1, 4, 5),
+       (3, 4, 2),
        (2, 2, 2);
--- SubjectID 2 and StudentID 2 exist
 
 #1
 SELECT *
